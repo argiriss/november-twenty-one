95685395922fe5813ab31908737451539fcc1d25
Argiris A <anastasakis.an@gmail.com>
2017-11-21 15:57:40 +0200

    Initializes project

---
+++ TwentyOneNovember/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwentyOneNovember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> list3 = new List<double> { 1, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 1, 2, 2,2,2, 3, 4, 5, 6, };
+            List<string> list = new List<string>{  "as", "as", "as", "as", "as", "ads"};
+            Utilities.LongestSubsequence(list);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TwentyOneNovember/Utilities.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOneNovember
+{
+    static class Utilities
+    {
+        public static void SwapInt(ref int a, ref int b)
+        {
+            int temp=a;
+            a = b;
+            b = temp;
+        }
+
+
+        public static void SwapGen<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static List<double> LongestSubsequence(List<double> list)
+        {
+            List<double> List2=new List<double>();
+            int temp = 0;
+            int count = 1;
+            double max = list[0];
+            for(int i=0; i<list.Count-1; i++)
+            {
+                if (list[i] == list[i + 1])
+                {
+                    count++;
+                    if (i == list.Count - 2)
+                    {
+                        if (count > temp)
+                        {
+                            max = list[i];
+                            temp = count;
+                        }
+                    }
+                }
+                else
+                {
+                    if (count > temp)
+                    {
+                        temp = count;
+                        count = 1;
+
+                    }
+                    else
+                    {
+                        count = 1;
+                    }
+                }
+
+            }
+
+            for (int y = 0; y < temp; y++)
+            {
+                List2.Add(max);
+                Console.WriteLine(max);
+            }
+            
+            return List2;
+        }
+
+        //auto einai to sosto
+        public static List<T> LongestSubsequence<T>(List<T> list)
+        {
+            List<T> List2 = new List<T>();
+            int temp = 0;
+            int count = 1;
+            T max = list[0];
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i].Equals(list[i + 1]))
+                {
+                    count++;
+                    if (i== list.Count - 2)
+                    {
+                        if (count > temp)
+                        {
+                            max = list[i];
+                            temp = count;
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    if (count > temp)
+                    {
+                        max = list[i];
+                        temp = count;
+                        count = 1; 
+                    }
+                    else
+                    {
+                        count = 1;
+                    }
+                }
+            }
+
+            for (int y = 0; y < temp; y++)
+            {
+                List2.Add(max);
+                Console.WriteLine(max);
+            }
+
+            return List2;
+        }
+    }
+}
